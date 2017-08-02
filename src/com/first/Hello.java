3e080b5a595c57c4b3af1b98029c7c59a54d3cc2
mohuandao <1034255194@qq.com>
2017-08-02 23:06:38 +0800

    hello

---
+++ src/com/first/Hello.java
@@ -0,0 +1,15 @@
+package com.first;
+
+/**
+ * Created by wangdong on 2017/8/2.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.print("hello");
+        System.out.print("hello1");
+        System.out.print("hello2");
+        System.out.print("hello3");
+        System.out.println("a");
+
+    }
+}
